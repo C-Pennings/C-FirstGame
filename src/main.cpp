56a96466ac202553a0fce2f4f9eeb772aab4d815
C-pennings <charliepennings13@gmail.com>
2026-04-09 16:45:42 -0400

    First Commet

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//ctrl + shift + b to build exe file
+//F5 to execute script
+
+static bool running = true;
+
+int main(){
+    while (running){
+        //updating the game
+
+    }
+
+    return 0;
+}
